d7f0b7b7e759da16bfe66920c4e82cf6e4514f41
ThanhPham2611 <85149054+ThanhPham2611@users.noreply.github.com>
2021-07-15 10:25:31 +0700

    tuan6

---
+++ tuan6/bai4.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt("Nhập chiều rộng: ");
+let inputHeight = prompt("Nhập chiều dài: ");
+inputHeight = Number(inputHeight);
+inputWidth = Number(inputWidth);
+let Area = inputHeight*inputWidth;
+document.write(Area);
