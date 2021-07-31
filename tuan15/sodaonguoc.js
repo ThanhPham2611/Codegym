db9fa1652ca068b39d50e6ce97ee3a6a817e26d5
ThanhPham2611 <85149054+ThanhPham2611@users.noreply.github.com>
2021-07-31 18:26:29 +0700

    tuan15

---
+++ tuan15/sodaonguoc.js
@@ -0,0 +1,12 @@
+debugger
+let num = parseInt(prompt("Nhập số: "));
+console.log(num);
+let total = 0;
+let tmp;
+while (num>0){
+    tmp = num%10;
+    total = total*10 + tmp;
+    num = parseInt(num/10);
+}
+alert('Số đảo ngược là: ' + total);
+
